396be5f84144913172429ba3294e6b9eae920379
eddie.chen <chenghung0510@gmail.com>
2018-05-29 22:22:01 +0800

    feat(model): [post] added

--- models/post.go
+++ models/post.go
@@ -8,45 +8,45 @@ import (
 	"github.com/gobuffalo/validate"
 )
 
-type User struct {
+type Post struct {
 	ID        int       `json:"id" db:"id"`
-	Email     string    `json:"email" db:"email"`
-	FirstName string    `json:"first_name" db:"first_name"`
-	LastName  string    `json:"last_name" db:"last_name"`
+	Title     string    `json:"title" db:"title"`
+	Content   string    `json:"content" db:"content"`
+	UserID    int       `json:"user_id" db:"user_id"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
-	Posts     Posts     `has_many:"posts" json:"-" order_by:"id:asc"`
+	User      User      `belongs_to:"user" json:"-"`
 }
 
 // String is not required by pop and may be deleted
-func (u User) String() string {
-	ju, _ := json.Marshal(u)
-	return string(ju)
+func (p Post) String() string {
+	jp, _ := json.Marshal(p)
+	return string(jp)
 }
 
-// Users is not required by pop and may be deleted
-type Users []User
+// POSTs is not required by pop and may be deleted
+type Posts []Post
 
 // String is not required by pop and may be deleted
-func (u Users) String() string {
-	ju, _ := json.Marshal(u)
-	return string(ju)
+func (p Posts) String() string {
+	jp, _ := json.Marshal(p)
+	return string(jp)
 }
 
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // This method is not required and may be deleted.
-func (u *User) Validate(tx *pop.Connection) (*validate.Errors, error) {
+func (p *Post) Validate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.NewErrors(), nil
 }
 
 // ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
 // This method is not required and may be deleted.
-func (u *User) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+func (p *Post) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.NewErrors(), nil
 }
 
 // ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
 // This method is not required and may be deleted.
-func (u *User) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+func (p *Post) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.NewErrors(), nil
 }
---
+++ models/post_test.go
@@ -0,0 +1,9 @@
+package models
+
+import (
+	"testing"
+)
+
+func Test_POST(t *testing.T) {
+	t.Fatal("This test needs to be implemented!")
+}
