236647b1e6bb5d75e60ffb587acfae9bd847a4c1
eddie.chen <chenghung0510@gmail.com>
2018-05-31 00:00:43 +0800

    refactor(action): [Post] paginate by PaginateFromParams

--- actions/post.go
+++ actions/post.go
@@ -1,23 +1,18 @@
 package actions
 
 import (
-	"strconv"
-
 	"github.com/buffalo_learning/models"
 	"github.com/gobuffalo/buffalo"
 )
 
 // POSTIndex default implementation.
 func PostIndex(c buffalo.Context) error {
-	page, _ := strconv.Atoi(c.Param("page"))
-	size, _ := strconv.Atoi(c.Param("size"))
 	posts := models.Posts{}
 	tx := models.DB
 	c.Logger().Debug("find posts by user_id=", c.Param("user_id"))
-	c.Logger().Debug("page=", page, ", size=", size)
 	tx.
 		Where("user_id = ?", c.Param("user_id")).
-		Paginate(page, size).
+		PaginateFromParams(c.Params()).
 		All(&posts)
 
 	return c.Render(200, r.JSON(posts))
