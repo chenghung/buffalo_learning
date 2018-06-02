937bd66489af2d79030faee9aa3aeddc0d921ba9
eddie.chen <chenghung0510@gmail.com>
2018-06-02 22:30:23 +0800

    feat(model): [User] save encrypted password in db

--- models/user.go
+++ models/user.go
@@ -6,16 +6,19 @@ import (
 
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/validate"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID        int       `json:"id" db:"id"`
-	Email     string    `json:"email" db:"email"`
-	FirstName string    `json:"first_name" db:"first_name"`
-	LastName  string    `json:"last_name" db:"last_name"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
-	Posts     Posts     `has_many:"posts" json:"-" order_by:"id:asc"`
+	ID                int       `json:"id" db:"id"`
+	Email             string    `json:"email" db:"email"`
+	Password          string    `json:"password" db:"-"`
+	EncryptedPassword string    `json:"-" db:"encrypted_password"`
+	FirstName         string    `json:"first_name" db:"first_name"`
+	LastName          string    `json:"last_name" db:"last_name"`
+	CreatedAt         time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at" db:"updated_at"`
+	Posts             Posts     `has_many:"posts" json:"-" order_by:"id:asc"`
 }
 
 // String is not required by pop and may be deleted
@@ -49,4 +52,18 @@ func (u *User) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
 // This method is not required and may be deleted.
 func (u *User) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.NewErrors(), nil
+}
+
+// BeforeSave gets run while you call create, update or save
+func (u *User) BeforeSave(tx *pop.Connection) error {
+	if u.Password != "" {
+		ep, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return err
+		}
+
+		u.EncryptedPassword = string(ep)
+	}
+
+	return nil
 }
